63a1816c6d26d3ceb46e78f3407add7c26ef6432
Kamal Shah <kkamal714@gmail.com>
2020-10-14 18:56:25 +0530

    [Kamal] | Adds Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React from 'react'
+import Posts from './components/Posts'
+import { Box } from '@material-ui/core'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import SinglePost from './components/SinglePost'
 
 function App() {
+	return (
+		<Box>
+			<Provider store={store}>
+				<BrowserRouter>
+					<Switch>
+						<Route path='/' exact component={Posts} />
+						<Route path='/:id' exact component={SinglePost} />
+					</Switch>
+				</BrowserRouter>
+			</Provider>
+		</Box>
+	)
 }
 
-export default App;
+export default App
