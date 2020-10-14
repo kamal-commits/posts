77ec8693fd5fc5d59bfaaeea608d8f07deefb05c
Kamal Shah <kkamal714@gmail.com>
2020-10-14 18:52:43 +0530

    [Kamal] | Adds PostCard component

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import {
+	Box,
+	Button,
+	Card,
+	CardContent,
+	makeStyles,
+	Typography
+} from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+	cardStyle: {
+		height: '300px',
+		backgroundColor: '#cc33ff',
+		[theme.breakpoints.down('sm')]: {
+			height: '100%'
+		}
+	},
+	titleStyle: {
+		color: '#ffffff',
+		fontFamily: 'Patua One',
+		textDecoration: 'underline'
+	},
+	bodyStyle: {
+		color: '#ffffff',
+		paddingTop: '15px',
+		fontSize: '20px'
+	}
+}))
+
+const PostCard = ({ title, body, postId }) => {
+	const classes = useStyles()
+	return (
+		<Box>
+			<Card className={classes.cardStyle}>
+				<CardContent>
+					<Button style={{ color: '#ffffff' }}>POST : {postId}</Button>
+					<Typography variant='h5' align='center' className={classes.titleStyle}>
+						{title}
+					</Typography>
+					<Typography className={classes.bodyStyle}> {body} </Typography>
+				</CardContent>
+			</Card>
+		</Box>
+	)
+}
+
+export default PostCard
